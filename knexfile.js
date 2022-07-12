6bce9164e62cd29fbd995f8716788fa6edb322d8
Brian Morgan <brianwmorgan317@gmail.com>
2022-07-12 13:22:51 -0400

    updated folders and files as specified in lesson 39.4

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
+const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
@@ -11,5 +12,8 @@ const { DATABASE_URL } = process.env;
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
   },
 };
